abdd5ed2188f486038726018738ce03e414d3aa8
Jordan T. Gray <business@shinkson47.in>
2021-03-19 19:05:28 +0000

    Created project
    - added empty main class,
    - adds OPEX project

---
+++ src/main.java
@@ -0,0 +1,25 @@
+package PACKAGE_NAME;/**
+  * <h1></h1>
+  * <br>
+  * <p>
+  *
+  * </p>
+  * @author <a href="https://www.shinkson47.in">Jordan T. Gray on 19/03/2021</a>
+  * @since v1
+  * @version 1 
+ */public class main {
+//#region constants
+//#endregion constants
+
+//#region fields
+//#endregion fields
+
+//#region constructors
+//#endregion constructors
+
+//#region operations
+//#endregion operations
+
+//#region static
+//#endregion static
+}
