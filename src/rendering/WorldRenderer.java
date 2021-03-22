d6833c8bcdacb8c0cb35edaf1a7167c3a71959b9
Jordan T. Gray <business@shinkson47.in>
2021-03-22 00:12:49 +0000

    Empty world renderer
    This will render a world, but for now just renders a grid of random tiles every second

--- src/rendering/WorldRenderer.java
+++ src/rendering/WorldRenderer.java
@@ -1,29 +1,22 @@
 package rendering;
 
-import de.matthiasmann.twl.utils.PNGDecoder;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-import org.lwjgl.opengl.GL30;
+import utility.Debug;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
+import java.util.Random;
 
 /**
- * <h1></h1>
+ * <h1>Renders a game world</h1>
  * <br>
  * <p>
- *
+ * but not yet, for now just displays a single quad for texture if 1
  * </p>
  *
  * @author <a href="https://www.shinkson47.in">Jordan T. Gray on 21/03/2021</a>
  * @version 1
  * @since v1
  */
-public class TileRenderer extends Renderer {
+public class WorldRenderer extends Renderer {
+
     //#region constants
     //#endregion constants
 
@@ -34,27 +27,23 @@ public class TileRenderer extends Renderer {
     //#endregion constructors
 
     //#region operations
-    private void texturedQuadTest(int texture,
-                                  float x,
-                                  float y,
-                                  float w,
-                                  float h)
-    {
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-        glPushMatrix();
-        glTranslatef(x, y, 0.0f);
-
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glEnable(GL_TEXTURE_2D);
-
-        glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f);    glVertex3f(x, y, 0.0f);
-        glTexCoord2f(0.0f, 1.0f);    glVertex3f(x, y + h, 0.0f);
-        glTexCoord2f(1.0f, 1.0f);    glVertex3f(x + w, y + h, 0.0f);
-        glTexCoord2f(1.0f, 0.0f);    glVertex3f(x + w, y, 0.0f);
-        glEnd();
-
-        glPopMatrix();
+    @Override
+    public void renderFrame() {
+        int cols = 4;
+        int rows = 20;
+        for (int y = rows; y >= 0; y--) {
+            for (int x = cols; x >= 0; x--) {
+                //renderQuad(Debug.debugValue, TILE_WIDTH * (x % cols) + + (((x / cols) % 2 != 0) ? 0 : (TILE_WIDTH / 2)), (TILE_HEIGHT / 2) * y);
+                renderQuad(Debug.random.nextInt(textures.size()) + 1, x * TILE_WIDTH + ((y % 2 == 0) ? TILE_WIDTH / 2 : 0), y * (TILE_HEIGHT / 2));
+            }
+        }
+        try {
+            synchronized(this) {
+                wait(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     //#endregion operations
 
