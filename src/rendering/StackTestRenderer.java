e85d83d79825d6cf3f0a5e6910776d2b8997c93f
Jordan T. Gray <business@shinkson47.in>
2021-03-25 16:08:40 +0000

    added test renderer

---
+++ src/rendering/StackTestRenderer.java
@@ -0,0 +1,25 @@
+package rendering;/**
+  * <h1></h1>
+  * <br>
+  * <p>
+  *
+  * </p>
+  * @author <a href="https://www.shinkson47.in">Jordan T. Gray on 24/03/2021</a>
+  * @since v1
+  * @version 1 
+ */public class StackTestRenderer {
+    //#region constants
+    //#endregion constants
+    
+    //#region fields
+    //#endregion fields
+    
+    //#region constructors
+    //#endregion constructors
+    
+    //#region operations
+    //#endregion operations
+    
+    //#region static
+    //#endregion static
+}
