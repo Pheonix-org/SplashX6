a91a793e43d3a352ee603d892af5f612913cc153
Dylan <dylanbrand773@gmail.com>
2021-03-25 21:52:23 +0000

    Tile class

---
+++ src/tiles/Tile.java
@@ -0,0 +1,50 @@
+package tiles;
+
+/**
+ * <h1></h1>
+ * <br>
+ * <p>
+ *
+ * </p>
+ *
+ * @author Dylan Brand 23/03/2021
+ * @version 1
+ */
+public class Tile {
+    //#region constants
+    public final String tileName;
+    public final String tileSetName;
+    public int cachedID;
+
+    // 4 strings that reperesent the quadrants of the tile, N/E/S/W
+    public final String north;
+    public final String east;
+    public final String south;
+    public final String west;
+
+    //#endregion constants
+
+    //#region fields
+    //#endregion fields
+
+    //#region constructors
+    public Tile(String TileName){
+        this.tileName = TileName;
+
+        tileSetName = TileName.substring(0, TileName.indexOf("."));
+        String[] tileNames = TileName.substring(TileName.indexOf(".") + 1).split("_");
+        north = tileNames[0];
+        east = tileNames[1];
+        south =tileNames[2];
+        west = tileNames[3];
+
+        cachedID = TileSet.FindTileTexture(TileName);
+    }
+    //#endregion constructors
+
+    //#region operations
+    //#endregion operations
+
+    //#region static
+    //#endregion static
+}
