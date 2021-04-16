826fd175ac9c8d61b74d57d5cfd7e35ccc7f7fcf
Jordan T. Gray <business@shinkson47.in>
2021-04-16 23:27:25 +0100

    Bit of test UI in game screen

--- core/src/com/shinkson47/SplashX6/rendering/screens/GameScreen.java
+++ core/src/com/shinkson47/SplashX6/rendering/screens/GameScreen.java
@@ -7,10 +7,18 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricStaggeredTiledMapRenderer;
-import com.shinkson47.SplashX6.input.KeyHandler;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.shinkson47.SplashX6.input.mouse.MouseHandler;
 import com.shinkson47.SplashX6.rendering.Camera;
+import com.shinkson47.SplashX6.utility.Assets;
 import com.shinkson47.SplashX6.world.World;
+
 
 /**
  * <h1>Main rendering class</h1>
@@ -41,6 +49,9 @@ public class GameScreen extends ScreenAdapter {
 
     private SpriteBatch batch = new SpriteBatch();
 
+    private Stage stage;
+    private Viewport viewport;
+
     //#region construction
 
 
@@ -50,14 +61,27 @@ public class GameScreen extends ScreenAdapter {
 
     public void create(){
         MouseHandler.create();
-
         sr = new ShapeRenderer();
         r = new IsometricStaggeredTiledMapRenderer(World.focusedWorld.getMap());
-
         resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        createUI();
     }
 
+    private void createUI(){
+        viewport = new ScalingViewport(Scaling.stretch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera.getCam());
+        stage = new Stage(viewport);
+
+        Gdx.input.setInputProcessor(stage);
+
+        Window w = new Window("", Assets.SKIN);
+        w.add(new Label("Fuck off", Assets.SKIN));
+        w.setResizable(true);
+        w.setMovable(true);
+
+        stage.addActor(w);
+        stage.addActor(new Label("double fuck off", Assets.SKIN));
+    }
     //#endregion construction
 
 
@@ -74,11 +98,15 @@ public class GameScreen extends ScreenAdapter {
         r.setView(camera.getCam().combined,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         camera.update();
         r.render();
+
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
         camera.resize(width, height);
+        //stage.getViewport().update(width, height, true);
     }
     //#endregion rendering operations
 
