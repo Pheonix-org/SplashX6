f68db0f0653ce030a5e0880114b14777fcfc0719
Jordan T. Gray <business@shinkson47.in>
2021-04-23 13:11:15 +0100

    Updated Debug window to use StageWindow
    And added a few more options

--- core/src/com/shinkson47/SplashX6/utility/Debug.java
+++ core/src/com/shinkson47/SplashX6/utility/Debug.java
@@ -2,14 +2,14 @@ package com.shinkson47.SplashX6.utility;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
-import com.shinkson47.SplashX6.rendering.Camera;
-import com.shinkson47.SplashX6.world.World;
 import com.shinkson47.SplashX6.game.GameHypervisor;
+import com.shinkson47.SplashX6.rendering.StageWindow;
+import com.shinkson47.SplashX6.rendering.screens.GameScreen;
+import com.shinkson47.SplashX6.world.World;
 
 /**
  * <h1>Random playground for test / development scripture</h1>
@@ -23,58 +23,86 @@ import com.shinkson47.SplashX6.game.GameHypervisor;
  * @since v1
  */
 public class Debug {
-    public static Boolean debug = false;
-    private static SpriteBatch batch;
-    private static Camera camera;
-    private static ShapeRenderer sr;
-    private static BitmapFont font;
+
+    /**
+     * Debug feature switches
+     */
+    public static boolean MouseInfo;
+
+
+    public static final DebugWindow MainDebugWindow = new DebugWindow();
+    public static GameScreen gameRenderer;
 
     public static void create(){
-        camera = GameHypervisor.getGameRenderer().getCam();
-        batch  = GameHypervisor.getGameRenderer().getBatch();
-        sr     = GameHypervisor.getGameRenderer().getSr();
-        font   = GameHypervisor.getGameRenderer().getFont();
+        gameRenderer = GameHypervisor.getGameRenderer();
+        gameRenderer.getHUDStage().addActor(MainDebugWindow);
     }
 
     public static void update(){
-        if (!debug) return;
+        renderMouseInfo();
+    }
+
+    public static void dispose(){
+        if (!MouseInfo) return;
+    }
+
+    public static class DebugWindow extends StageWindow {
+
+        public DebugWindow() {
+            super("DEBUG", false);
+        }
+
+        /**
+         * <h2>Constructs the content to be displayed in this window</h2>
+         */
+        @Override
+        protected void constructContent() {
+
+            addButton("Toggle Tile interpolation", o -> World.focusedWorld.swapTiledInterp());
+            addButton("Experimental : Toggle Camera Pan Tilt", o -> GameHypervisor.getGameRenderer().getCam().enableMoveTilt = !GameHypervisor.getGameRenderer().getCam().enableMoveTilt);
+            addButton("Experimental : Toggle Camera Zoom Tilt", o -> GameHypervisor.getGameRenderer().getCam().enableZoomTilt = !GameHypervisor.getGameRenderer().getCam().enableZoomTilt);
+            addButton("Rotate camera +", o -> GameHypervisor.getGameRenderer().getCam().getCam().rotate(10,0,0,1));
+            addButton("Rotate camera -", o -> GameHypervisor.getGameRenderer().getCam().getCam().rotate(-10,0,0,1));
+
+            addButton("Toggle Cursor Info", o -> MouseInfo = !MouseInfo);
+            addButton("Hide", o -> toggleShown());
+        }
+    }
+
+    /**
+     * <h2>Renders mouse information to the game screen.</h2>
+     */
+    private static void renderMouseInfo(){
+        if (!MouseInfo) return;
 
         int mousex = Gdx.input.getX();
         int mousey = Gdx.input.getY();
         int rendery = Gdx.graphics.getHeight() - mousey;
 
-        Vector3 WorldSpace = camera.getCam().unproject(new Vector3(mousex, mousey,0));
+        Vector3 WorldSpace = GameHypervisor.getGameRenderer().getCam().getCam().unproject(new Vector3(mousex, mousey,0));
         Vector3 MapSpace = World.WorldspaceToMapspace((int)WorldSpace.x, (int)WorldSpace.y);
 
-        batch.begin();
-        font.draw(batch, "Mouse Raw : x:" + mousex + ", y:" + mousey, mousex + 20, rendery - 20);
-        font.draw(batch, "WorldSpace : " + WorldSpace, mousex + 20, rendery);
-        font.draw(batch, "MapSpace : " + MapSpace, mousex + 20, rendery + 20);
-        font.draw(batch, "HitTest : " + World.hittestResult, mousex + 20, rendery + 40);
-        batch.end();
+        gameRenderer.getHUDBatch().begin();
+        gameRenderer.getFont().draw(gameRenderer.getHUDBatch(), "Mouse Raw : x:" + mousex + ", y:" + mousey, mousex + 20, rendery - 20);
+        gameRenderer.getFont().draw(gameRenderer.getHUDBatch(), "WorldSpace : " + WorldSpace, mousex + 20, rendery);
+        gameRenderer.getFont().draw(gameRenderer.getHUDBatch(), "MapSpace : " + MapSpace, mousex + 20, rendery + 20);
+        gameRenderer.getFont().draw(gameRenderer.getHUDBatch(), "HitTest : " + World.hittestResult, mousex + 20, rendery + 40);
+        gameRenderer.getHUDBatch().end();
 
         if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            sr.setColor(0,1,0,1);
+            gameRenderer.getSr().setColor(0,1,0,1);
             ((TiledMapTileLayer) World.focusedWorld.getMap().getLayers().get(0)).setCell((int) MapSpace.x, (int) MapSpace.y, null);
         } else
-            sr.setColor(1,1,1,1);
+            gameRenderer.getSr().setColor(1,1,1,1);
 
 
-        sr.begin(ShapeRenderer.ShapeType.Line);
-        sr.line(0,rendery,0,Gdx.graphics.getWidth(),rendery,0);
-        sr.line(mousex,0,0,mousex,Gdx.graphics.getHeight(),0);
-        sr.end();
+        gameRenderer.getSr().begin(ShapeRenderer.ShapeType.Line);
+        gameRenderer.getSr().line(0,rendery,0,Gdx.graphics.getWidth(),rendery,0);
+        gameRenderer.getSr().line(mousex,0,0,mousex,Gdx.graphics.getHeight(),0);
+        gameRenderer.getSr().end();
 
-        sr.begin(ShapeRenderer.ShapeType.Filled);
-        sr.circle(mousex, rendery, 5);
-        sr.end();
-
+        gameRenderer.getSr().begin(ShapeRenderer.ShapeType.Filled);
+        gameRenderer.getSr().circle(mousex, rendery, 5);
+        gameRenderer.getSr().end();
     }
-
-    public static void dispose(){
-        if (!debug) return;
-
-    }
-
-
 }
