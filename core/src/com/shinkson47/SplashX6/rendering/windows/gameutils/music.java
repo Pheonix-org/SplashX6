22acf09ce250731348725d04a4ada6579e1f12f5
Dylan <dylanbrand773@gmail.com>
2021-06-29 20:05:20 +0100

    Small change for music UI (not complete, obviously)
    Uploading "minor" additions as re-cloning repo.

--- core/src/com/shinkson47/SplashX6/rendering/windows/gameutils/music.java
+++ core/src/com/shinkson47/SplashX6/rendering/windows/gameutils/music.java
@@ -2,8 +2,10 @@ package com.shinkson47.SplashX6.rendering.windows.gameutils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.utils.Array;
+import com.shinkson47.SplashX6.audio.AudioController;
 import com.shinkson47.SplashX6.rendering.StageWindow;
 import com.shinkson47.SplashX6.utility.Assets;
 import com.shinkson47.SplashX6.utility.Utility;
@@ -17,16 +19,12 @@ import java.util.Map;
 public class music extends StageWindow {
 
     // Fields
-   Map<String, Object> tracks = Plist.fromXml(Gdx.files.internal("core/assets/audio/data/playlists.plist").readString());
-   private Array<String> playlist = new Array<>();
+    Map<String, Object> tracks = Plist.fromXml(Gdx.files.internal("core/assets/audio/data/playlists.plist").readString());
+    private Array<String> playlist = new Array<>();
 
-   // Constructors
+    // Constructors
     public music() throws XmlParseException, IOException {
         super("Music");
-
-
-
-
         constructContent();
     }
 
@@ -39,16 +37,26 @@ public class music extends StageWindow {
 
         top();
 
-        label("playing").padBottom(20);
-        label("songTest").padBottom(20).row();
+        // Displays the currently selected track
+        label("playing").padTop(80);
+        label("songTest").padTop(80).row();
 
-        playlist.addAll(Utility.CollectionToGDXArray(((Map)tracks.get("playlist one")).values()));
+        playlist.addAll(Utility.CollectionToGDXArray(((Map) tracks.get("playlist one")).values()));
 
         SelectBox pList = new SelectBox<String>(Assets.SKIN);
-        //pList.setItems(playlist);
-        add(pList).width(400).colspan(2);
+        pList.setItems(playlist);
+        add(pList).width(400).colspan(2).padTop(20).row();
+
+//        Button btnSkip = new Button();
+//        btnSkip.setName("Skip");
+//
+//        add(btnSkip);
+
+        // Button controls for track management (Current actioms are for development)
+        addButton("skipTrack", e -> AudioController.muteAudio()).pad(20, 0, 20, 0);
+        addButton("pauseTrack", e -> AudioController.pauseMusic());
 
     }
-
-
 }
+
+
