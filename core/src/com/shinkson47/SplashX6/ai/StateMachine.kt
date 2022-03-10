18dc73ebca8750a5ef117f7429f5c551d5fd367a
Jordan T. Gray <business@shinkson47.in>
2022-03-10 12:29:54 +0000

    Window to look at state machines

--- core/src/com/shinkson47/SplashX6/ai/StateMachine.kt
+++ core/src/com/shinkson47/SplashX6/ai/StateMachine.kt
@@ -52,6 +52,14 @@ import java.util.function.Predicate
  */
 open class StateMachine : Runnable {
 
+    companion object {
+        val activeMachines = ArrayList<StateMachine>()
+    }
+
+    init { activeMachines.add(this) }
+    protected fun finalize() =
+        activeMachines.remove(this)
+
     /**
      * ## If true, machine logs every state change.
      * False by default.
@@ -61,7 +69,7 @@ open class StateMachine : Runnable {
     /**
      * ## All states in this state machine.
      */
-    private val states = ArrayList<State>()
+    val states = ArrayList<State>()
 
     /**
      * ## Returns a given state from [states].
@@ -77,10 +85,10 @@ open class StateMachine : Runnable {
      *
      * TODO if not set, automatically inherited from default.
      */
-    private var currentState: State
+    var currentState: State
         get() = _currentState ?: throw UninitializedPropertyAccessException("\"tmp\" was queried before being initialized")
 
-        set(value) {
+        private set(value) {
             checkStateExists(value)
             _currentState = value
         }
@@ -250,8 +258,8 @@ open class StateMachine : Runnable {
      * [StateMachine.State.enter] on [index].
      *
      */
-    private fun switchState(newState: StateMachine.State) {
-        println("Switch state $currentState to $newState")
+    fun switchState(newState: StateMachine.State) {
+        print("Switch state $currentState to $newState")
         checkStateExists(newState)
 
         currentState.exit()
